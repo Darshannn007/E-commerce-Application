0e5b4c9c0df9c847cd0cfbf5f06785dec9d87602
DARSHAN <darshandesale143@gmail.com>
2026-06-09 10:44:12 +0530

    deleted unnecessary files

--- flipkart/src/main/java/com/example/flipkart/repository/ReviewRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.flipkart.repository;
-
-import com.example.flipkart.Entity.ReviewEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<ReviewEntity,Long> {
-
-}
